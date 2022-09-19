d695e7b1d57617ef36bb6b3bdbec2588a9c9a8fe
Gresely05 <jordan.gresely@gmail.com>
2022-09-19 10:33:18 -0500

    Creacion de programa para comparar

---
+++ compara.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b;
+	cout<<"Ingrese a,b"<<endl;
+	cin>>a>>b;
+	if(a>b){
+		cout<<a<<" Es mayor a "<<b<<endl;
+	}else{
+		cout<<b<<" Es mayor a "<<a<<endl;
+	}
+	return 0;
+}
+
---
+++ suma.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c;
+	cout<<"Por favor ingrese 2 numeros"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"El resultado de la suma es: "<<c;
+	return 0;
+}
